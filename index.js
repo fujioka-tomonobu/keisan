e1f78c4fc3c9ded2a2fe39e1804af198e8b69386
Tomonobu Fujioka <fujioka.tomonob@fujitsu.com>
2025-09-21 22:10:51 +0900

    mod

--- index.js
+++ index.js
@@ -194,6 +194,7 @@ var event = new function(){
 		$('#game').show();
 		$('#next').hide();
 		$('#answer').hide();
+		$('#game-text').show();
 		
 		何問目 = 0;
 		カービ移動回数 = 0;
@@ -345,7 +346,7 @@ var event = new function(){
 
 			clearInterval(dededeAnimeId);
 			
-			if(カービ移動回数 > デデデ移動回数) {
+			if(カービ移動回数 >= デデデ移動回数) {
 				event.win();
 			}else{
 				event.lose();
